746bc139ec77706c6f6646a4252a37ef6a3faacd
Dave Parfitt <dparfitt@getchef.com>
2014-09-23 21:39:11 -0400

    working on proper test

--- test/proper_tests.erl
+++ test/proper_tests.erl
@@ -9,9 +9,12 @@
 -include_lib("proper/include/proper.hrl").
 -include_lib("eunit/include/eunit.hrl").
 -include("../src/pooler.hrl").
+-compile([export_all]).
+
+-behaviour(proper_statem).
 
 
--record(state, {worker_ids=[] }).
+-record(state, {worker_ids=[]}).
 
 initial_state() ->
     #state{}.
@@ -20,10 +23,8 @@ initial_state() ->
 gen_worker_id(S) ->
     elements(S#state.worker_ids).
 
-
-
 command(#state{worker_ids=[]}) ->
-    ok;
+   {call, pooler, take_member, [pool1]};
 command(S) ->
     oneof([
                 {call, pooler, take_member, [pool1]},
@@ -32,44 +33,49 @@ command(S) ->
 
 precondition(_S, {call, pooler, take_member, [pool1]}) ->
     true;
-precondition(_S, {call, pooler, return_member, [pool1, _WorkerId, _WorkerState]}) ->
-    true.
+precondition(S, {call, pooler, return_member, [pool1, WorkerId, _WorkerState]}) ->
+    lists:member(WorkerId, S#state.worker_ids).
 
-
-postcondition(_S, {call, pooler, take_member, [pool1]}, Res) ->
-    io:format(user, "TAKE MEMBER -> ~p~n",[Res]),
+postcondition(_S, {call, pooler, take_member, [pool1]}, _Res) ->
     true;
-postcondition(_S, {call, pooler, return_member, [pool1, WorkerId, WorkerState]}, Res) ->
-    io:format(user, "Info = ~p ~p ~p~n", [WorkerId, WorkerState, Res]),
+postcondition(_S, {call, pooler, return_member, [pool1, _WorkerId,
+                                                 _WorkerState]}, _Res) ->
     true.
 
-next_state(S, _V, {call, _, _, _}) ->
-    S.
+next_state(S, V, {call, pooler, take_member, _}) ->
+    S#state{worker_ids = [V|S#state.worker_ids]};
+
+next_state(S, _V, {call, pooler, return_member, [_, WorkerId, _]}) ->
+    S#state{worker_ids = lists:delete(WorkerId, S#state.worker_ids)}.
 
 prop_pooler() ->
-
-    Pools = 
-       [ 
-        [{name, pool1},
-            {max_count, 5},
-            {init_count, 2},
-            {start_mfa,
-            {pooled_gs, start_link, [{"p1"}]}}],
-
-        [{name, pool2},
-            {max_count, 5},
-            {init_count, 2},
-            {start_mfa,
-            {pooled_gs, start_link, [{"p2"}]}}]
-            ],
-    application:set_env(pooler, pools, Pools),
-    io:format(user, "Starting pooler statem~n", []),
     ?FORALL(Cmds, commands(?MODULE, initial_state()),
                 begin
-                    {_Seq,_P,ok} = run_commands(?MODULE, Cmds)
-                end),
-    true.
+                    % this is NOT correct
+                    Pools = 
+                    [ 
+                        [{name, pool1},
+                            {max_count, 5},
+                            {init_count, 2},
+                            {start_mfa,
+                            {pooled_gs, start_link, [{"p1"}]}}],
+
+                        [{name, pool2},
+                            {max_count, 5},
+                            {init_count, 2},
+                            {start_mfa,
+                            {pooled_gs, start_link, [{"p2"}]}}]
+                            ],
+                    application:set_env(pooler, pools, Pools),
+                    application:start(pooler),
+                    {_Seq,_P,ok} = run_commands(?MODULE, Cmds),
+                    true
+                end).
 
 proper_test() ->
-    ?assertEqual(true, proper:quickcheck(prop_pooler(), [{to_file, user}])).
+    io:format(user, "Starting pooler statem~n", []),
+    proper:quickcheck(prop_pooler(), [{to_file, user}]),
+    io:format(user, "pooler statem finished~n", []),
+    true.
+
 
